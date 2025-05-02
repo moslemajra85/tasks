f8a68df8ccfffb904c9d6846533ecd26e06cde07
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-05-02 11:49:18 +0100

    Add initial implementation of Todo List application with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,34 @@
+const todoInput = document.getElementById('task');
+const addButton = document.getElementById('btn-add');
+const todoList = document.getElementById('todo-list');
+
+addButton.addEventListener('click', function () {
+  if (todoInput.value === '') {
+    return;
+  }
+
+  // extract the value from the input field
+  const todoText = todoInput.value;
+
+  // create a new list item
+  const listItem = document.createElement('li'); //<li></li>
+  listItem.className = 'todo-item'; //<li class="todo-item"></li>
+  listItem.textContent = todoText; // <li>...</li>
+
+  // create a delete button
+  const deleteButton = document.createElement('button'); //<button></button>
+  deleteButton.className = 'delete-button'; //<button class="delete-button"></button>
+  deleteButton.textContent = 'Delete'; // <button>Delete</button>
+
+  listItem.appendChild(deleteButton); // <li>...</li><button>Delete</button>
+
+  // add  the created list item to the todo list
+  todoList.appendChild(listItem); // add the list item to the todo list
+
+  deleteButton.addEventListener('click', function () {
+    // remove the list item from the todo list
+    todoList.removeChild(listItem); // remove the list item from the todo list
+  });
+
+  todoInput.value = ''; // clear the input field
+});
